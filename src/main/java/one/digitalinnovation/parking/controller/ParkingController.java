1b411fd21092c2dfb362d32ba3f9606914c1f4de
Pedro Cardoso <pedrohfsc96@gmail.com>
2022-10-23 16:26:17 -0300

    1

---
+++ src/main/java/one/digitalinnovation/parking/controller/ParkingController.java
@@ -0,0 +1,25 @@
+package one.digitalinnovation.parking.controller;
+
+import one.digitalinnovation.parking.model.Parking;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class ParkingController {
+
+    @GetMapping
+    public List<Parking> findAll(){
+        var parking = new Parking();
+        parking.setColor("PRETO");
+        parking.setLicense("MISS-1111");
+        parking.setModel("VW GOL");
+        parking.setState("SP");
+
+        return Arrays.asList(parking, parking);
+
+    }
+
+}
