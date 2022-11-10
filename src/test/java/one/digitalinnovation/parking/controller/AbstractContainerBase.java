4a1559ec289481f2891f6bc7633cc8e751c58dda
Pedro Cardoso <pedrohfsc96@gmail.com>
2022-11-10 10:40:02 -0300

    Finalizando

--- src/test/java/one/digitalinnovation/parking/controller/AbstractContainerBase.java
+++ src/test/java/one/digitalinnovation/parking/controller/AbstractContainerBase.java
@@ -3,7 +3,7 @@ import org.testcontainers.containers.PostgreSQLContainer;
 
 public abstract class AbstractContainerBase {
 
-    static final PostgreSQLContainer POSTGRE_SQL_CONTAINER = null;
+    static PostgreSQLContainer POSTGRE_SQL_CONTAINER = null;
 
     static {
         POSTGRE_SQL_CONTAINER =  new PostgreSQLContainer("postgres:10-alpine");
